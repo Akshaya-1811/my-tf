a4dea42d00acbf20b5c17c3dbea619d7ecb7233f
akshaya-1811 <akshayapulluru18@gmail.com>
2024-01-30 21:08:20 +0530

    web subnte

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "myntra_vpc" {
     Name = "myntra"
   }
 }
+
+#Wed subnet
+resource "aws_subnet" "myntra-web-sn" {
+  vpc_id     = aws_vpc.myntra_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "myntra-web-subnet"
+  }
+}
