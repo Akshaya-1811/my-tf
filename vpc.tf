2638902ce255fda527d590f57128c37f7ec0c316
akshaya-1811 <akshayapulluru18@gmail.com>
2024-01-30 21:22:50 +0530

    db subnet

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "myntra_vpc" {
   }
 }
 
-#Wed subnet
+#web subnet
 resource "aws_subnet" "myntra-web-sn" {
   vpc_id     = aws_vpc.myntra_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -17,5 +17,17 @@ resource "aws_subnet" "myntra-web-sn" {
 
   tags = {
     Name = "myntra-web-subnet"
+  }
+}
+
+#db subnet
+resource "aws_subnet" "myntra-db-sn" {
+  vpc_id     = aws_vpc.myntra_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "myntra-database-subnet"
   }
 }
