545474cb83bfb3e6f21596e9710fb9ffcd53c041
akshaya-1811 <akshayapulluru18@gmail.com>
2024-01-31 20:47:19 +0530

    create database security group

--- vpc.tf
+++ vpc.tf
@@ -178,3 +178,37 @@ resource "aws_security_group" "myntra-web-sg" {
   }
 }
 
+# database security group
+resource "aws_security_group" "myntra-db-sg" {
+  name        = "myntra-db-traffic"
+  description = "Allow SSH - postgres inbound traffic"
+  vpc_id      = "${aws_vpc.myntra_vpc.id}"
+
+  ingress {
+    description = "SSH from vpc"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+   ingress {
+    description = "HTTP from vpc"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+     cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "myntra-database-sg"
+  }
+}
+
